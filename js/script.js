179d5ab6d425baef254ef5831b1c2dc7f04d2eed
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-26 18:47:21 +0100

    [UPD] Milestone 2 completed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 //vado a richiamare le variabili del dom
 const button = document.getElementById("btn");
 
-
+//richiamo le variabili per le card
+let passenger = document.getElementById("passengername")
+let distance = document.getElementById("distance")
+let Age = document.getElementById("passengerage")
+let finalCost = document.getElementById("finalcost")
 //corpo del programma
 button.addEventListener("click", function (event) {
     event.preventDefault();
     const elementKilometer = document.getElementById("km").value;
     const elementUserAge = document.getElementById("age").value;
+    const elementName = document.getElementById("name")
     let finalPrice = (elementKilometer * 0.21).toFixed(2);
-
+//andiamo a definire il calcolo 
     if (elementUserAge > 65) {
         finalPrice = (finalPrice * 0.6).toFixed(2);
-    }else if (elementUserAge < 18) {
+        Age.innerHTML = "Sconto del 40%";
+    }
+    else if (elementUserAge < 18) {
         finalPrice = (finalPrice * 0.8).toFixed(2);
+        Age.innerHTML = "Sconto del 20%"
     }
 
+    else{
+        Age.innerHTML = "tariffa normale"
+    }
+
+
+//andiamo a inserire i valori dentro le card
+passenger.innerHTML=elementName.value;
+distance.innerHTML= elementKilometer;
+finalCost.innerHTML = finalPrice;
 
 
 
